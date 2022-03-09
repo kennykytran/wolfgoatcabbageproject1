3538d04f6f6e7828edddf90ae9e4ac28e3d912e1
ktranfullerton2000 <badsprite201x@gmail.com>
2022-03-08 18:11:14 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,26 @@ class WolfGoatCabbage(Problem):
         super().__init__(initial, goal)
 
     def result(self, state, action):
-        if action[0] is not set():
-            return frozenset(state ^ action[0])
-        return set()
-        # current_state = state
-        # if state == {'F', 'W', 'G', 'C'}:
-        #     current_state = {'W', 'C'}
-        # elif state == {'W', 'C'}:
-        #     current_state = {'F', 'W', 'C'}
-        # elif state == {'F', 'W', 'C'}:
-        #     current_state = {'F', 'G', 'C'}
-        # elif state == {'F', 'G', 'C'}:
-        #     current_state = 'F', 'G', 'C'
-        # elif state == {'F', 'G', 'C'}:
-        #     current_state = {'G'}
-        # elif state == {'G'}:
-        #     current_state = {'F', 'G'}
-        # elif state == {'F', 'G'}:
-        #     current_state = set()
-        # return frozenset(current_state)
+        # if action[0] is not set():
+        #     return frozenset(state ^ action[0])
+        # return set()
+
+        current_state = state
+        if state == {'F', 'W', 'G', 'C'}:
+            current_state = {'W', 'C'}
+        elif state == {'W', 'C'}:
+            current_state = {'F', 'W', 'C'}
+        elif state == {'F', 'W', 'C'}:
+            current_state = {'C'}
+        elif state == {'C'}:
+            current_state = 'F', 'G', 'C'
+        elif state == {'F', 'G', 'C'}:
+            current_state = {'G'}
+        elif state == {'G'}:
+            current_state = {'F', 'G'}
+        elif state == {'F', 'G'}:
+            current_state = set()
+        return frozenset(current_state)
 
     def actions(self, state):
         if state == {'F', 'W', 'G', 'C'}:
@@ -34,7 +35,7 @@ class WolfGoatCabbage(Problem):
             return [{'F'}]
         elif state == {'F', 'W', 'C'}:
             return [{'F', 'W'}]
-        elif state == {'F', 'G', 'C'}:
+        elif state == {'C'}:
             return [{'F', 'G'}]
         elif state == {'F', 'G', 'C'}:
             return [{'F', 'C'}]
@@ -42,6 +43,8 @@ class WolfGoatCabbage(Problem):
             return [{'F'}]
         elif state == {'F', 'G'}:
             return [{'G', 'F'}]
+        else:
+            return [set()]
 
     def goal_test(self, state):
         return state == self.goal
