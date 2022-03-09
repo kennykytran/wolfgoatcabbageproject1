ee3df2ed39ba5ec92a2b4920a5188090261ef8b7
Minh Nguyen <minhnguyenandrew@gmail.com>
2022-03-08 18:39:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 from search import *
 
-//test
+
 class WolfGoatCabbage(Problem):
 
     def __init__(self, initial={'F', 'G', 'W', 'C'}, goal=set()):
         super().__init__(initial, goal)
 
+    def goal_test(self, state):
+        return state == self.goal
+    
     def result(self, state, action):
         return [{'F', 'G'}]
 
     def actions(self, state):
+        #1st level
         if state == {'F', 'W', 'G', 'C'}:
             return [{'F', 'G'}]
+        #2nd level
+        elif  state == {'F', 'G'}:
+            return [{'G'}]
+        #3rd level
+        elif  state == {'G'}:
+            return [{'F', 'W', 'G'}]
+        elif  state == {'G'}:
+            return [{'F', 'G', 'C'}]
+        #4th level
+        elif  state == {'F', 'W', 'G'}:
+            return [{'W'}]
+        elif  state == {'F', 'G', 'C'}:
+            return [{'C'}]
+        #5th level
+        elif  state == {'W'}:
+            return [{'F', 'W', 'C'}]
+        elif  state == {'C'}:
+            return [{'F', 'W', 'C'}]
+        #6th level
+        elif  state == {'F', 'W', 'C'}:
+            return [{'W', 'C'}]
+        #7th level
+        elif  state == {'W', 'C'}:
+            return [{'F', 'W', 'G', 'C'}]
 
-    def goal_test(self, state):
-        return state == self.goal
 
 
 def main():
-    puzzler = EightPuzzle((1, 2, 3, 4, 5, 6, 7, 8, 0))
-    print("Question 5")
-    print(puzzler.actions(state=(0, 1, 2, 3, 4, 5, 6, 7, 8)))
-    print("Question 6")
-    print(puzzler.result(state=(0, 1, 2, 3, 4, 5, 6, 7, 8), action='DOWN'))
-    print("Question 7")
-    puzzle = EightPuzzle((1, 2, 3, 5, 7, 4, 8, 6, 0))
-    print(breadth_first_graph_search(puzzle).solution())
-    print(len((breadth_first_graph_search(puzzle).solution())))
-
     wgc = WolfGoatCabbage()
+    print("Initial state")
     print(wgc.initial)
-    print(wgc.actions({'F', 'W', 'G', 'C'}))
+
+    # print("Actions")
+    # print(wgc.actions({'F', 'W', 'G', 'C'}))
+    
+    print("Goal Test")
+    # print(wgc.goal_test({'F', 'G', 'W', 'C'}))
+    
     # solution = depth_first_graph_search(wgc).solution()
     # print(solution)
     # solution = breadth_first_graph_search(wgc).solution()
